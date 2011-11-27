1f3bfd676986758fd4404b101c0399d83078c4c6
Peter Gill <peter@majorsilence.com>
2011-11-27 14:16:36 -0330

    Add x86 and x64 targets to solutions.

--- Release-Builds/build-fyi-release-dot-net-2.bat
+++ Release-Builds/build-fyi-release-dot-net-2.bat
@@ -4,27 +4,27 @@ REM /p:Configuration="Debug" or "Release"
 
 REM ************* Begin ANY CPU *********************************************
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\DataProviders\DataProviders.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\DataProviders\DataProviders.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-REM "C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "..\%CD%\..\OracleSP\OracleSp.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+REM "C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "..\%CD%\..\OracleSP\OracleSp.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlAsp\RdlAsp.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlAsp\RdlAsp.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlCmd\RdlCmd.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlCmd\RdlCmd.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlCri\RdlCri.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlCri\RdlCri.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlDesign\RdlDesign.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlDesign\RdlDesign.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlDesktop\RdlDesktop.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlDesktop\RdlDesktop.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlEngine\RdlEngine.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlEngine\RdlEngine.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlMapFile\RdlMapFile.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlMapFile\RdlMapFile.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlViewer\RdlViewer.sln" /toolsversion:2.0 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlViewer\RdlViewer.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="Any CPU" /t:clean;rebuild /m:4
 
-REM ************* End ANY CPU *********************************************
+
 
 
 del .\my-fyi-build-dot-net-2 /Q
@@ -46,4 +46,51 @@ copy ..\RdlViewer\RdlReader\bin\Release\RdlReader.exe .\my-fyi-build-dot-net-2\R
 
 7za.exe a my-fyi-build-dot-net-2.zip my-fyi-build-dot-net-2\
 
+REM ************* End ANY CPU *********************************************
 
+
+REM ************* Begin x86 *********************************************
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\DataProviders\DataProviders.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+REM "C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "..\%CD%\..\OracleSP\OracleSp.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlAsp\RdlAsp.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlCmd\RdlCmd.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlCri\RdlCri.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlDesign\RdlDesign.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlDesktop\RdlDesktop.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlEngine\RdlEngine.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlMapFile\RdlMapFile.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+"C:\Windows\Microsoft.NET\Framework\v3.5\MSBuild.exe" "%CD%\..\RdlViewer\RdlViewer.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="x86" /t:clean;rebuild /m:4
+
+
+
+
+del .\my-fyi-build-dot-net-2-x86 /Q
+mkdir .\my-fyi-build-dot-net-2-x86
+
+copy ..\DataProviders\bin\x86\Release\DataProviders.dll .\my-fyi-build-dot-net-2-x86\DataProviders.dll /Y
+REM copy ..\OracleSp\bin\x86\Release\OracleSp.dll .\my-fyi-build-dot-net-2-x86\OracleSp.dll /Y
+copy ..\RdlAsp\bin\x86\Release\RdlAsp.dll .\my-fyi-build-dot-net-2-x86\RdlAsp.dll /Y
+copy ..\RdlCmd\bin\x86\Release\RdlCmd.exe .\my-fyi-build-dot-net-2-x86\RdlCmd.exe /Y
+copy ..\RdlCri\bin\x86\Release\RdlCri.dll .\my-fyi-build-dot-net-2-x86\RdlCri.dll /Y
+copy ..\RdlDesign\bin\x86\Release\RdlDesigner.exe .\my-fyi-build-dot-net-2-x86\RdlDesigner.exe /Y
+copy ..\RdlDesktop\bin\x86\Release\RdlDesktop.exe .\my-fyi-build-dot-net-2-x86\RdlDesktop.exe /Y
+copy ..\RdlDesktop\bin\x86\Release\config.xml .\my-fyi-build-dot-net-2-x86\config.xml /Y
+copy ..\RdlEngine\bin\x86\Release\RdlEngine.dll .\my-fyi-build-dot-net-2-x86\RdlEngine.dll /Y
+copy ..\RdlEngine\bin\x86\Release\RdlEngineConfig.xml .\my-fyi-build-dot-net-2-x86\RdlEngineConfig.xml /Y
+copy ..\RdlMapFile\bin\x86\Release\RdlMapFile.exe .\my-fyi-build-dot-net-2-x86\RdlMapFile.exe /Y
+copy ..\RdlViewer\bin\x86\Release\RdlViewer.dll .\my-fyi-build-dot-net-2-x86\RdlViewer.dll /Y
+copy ..\RdlViewer\RdlReader\bin\x86\Release\RdlReader.exe .\my-fyi-build-dot-net-2-x86\RdlReader.exe /Y
+
+7za.exe a my-fyi-build-dot-net-2-x86.zip my-fyi-build-dot-net-2-x86\
+
+REM ************* End x86 *********************************************
