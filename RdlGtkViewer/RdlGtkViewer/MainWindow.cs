1f6481a54ca5c70ca89e3824eae5bf65c42fbf71
Peter <peter@ubuntu.ubuntu-domain>
2011-12-30 21:19:10 -0330

    Cleanups.  Still no parameter support.

--- RdlGtkViewer/RdlGtkViewer/MainWindow.cs
+++ RdlGtkViewer/RdlGtkViewer/MainWindow.cs
@@ -6,6 +6,9 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		
+		this.rdlgtkviewer1.LoadReport(
+			@"/home/peter/Projects/My-FyiReporting/Examples/SqliteExamples/SimpleTest2.rdl");
 	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
--- RdlGtkViewer/RdlGtkViewer/gtk-gui/MainWindow.cs
+++ RdlGtkViewer/RdlGtkViewer/gtk-gui/MainWindow.cs
@@ -3,6 +3,9 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.Table table1;
+	private global::fyiReporting.RdlGtkViewer.RdlGtkViewer rdlgtkviewer1;
+    
 	protected virtual void Build ()
 	{
 		global::Stetic.Gui.Initialize (this);
@@ -10,10 +13,26 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.table1 = new global::Gtk.Table (((uint)(3)), ((uint)(3)), false);
+		this.table1.Name = "table1";
+		this.table1.RowSpacing = ((uint)(6));
+		this.table1.ColumnSpacing = ((uint)(6));
+		// Container child table1.Gtk.Table+TableChild
+		this.rdlgtkviewer1 = new global::fyiReporting.RdlGtkViewer.RdlGtkViewer ();
+		this.rdlgtkviewer1.Events = ((global::Gdk.EventMask)(256));
+		this.rdlgtkviewer1.Name = "rdlgtkviewer1";
+		this.table1.Add (this.rdlgtkviewer1);
+		global::Gtk.Table.TableChild w1 = ((global::Gtk.Table.TableChild)(this.table1 [this.rdlgtkviewer1]));
+		w1.TopAttach = ((uint)(1));
+		w1.BottomAttach = ((uint)(2));
+		w1.LeftAttach = ((uint)(1));
+		w1.RightAttach = ((uint)(2));
+		this.Add (this.table1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
-		this.DefaultWidth = 400;
+		this.DefaultWidth = 487;
 		this.DefaultHeight = 300;
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
