2a89a7941fe1cb5f4cf1e99109aa7799550358b8
Peter Gill <peter@majorsilence.com>
2012-04-21 19:39:25 -0230

    Initial QrCode class.

---
+++ RdlCri/QrCode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using fyiReporting.RDL;
+
+namespace fyiReporting.CRI
+{
+    public class QrCode : ICustomReportItem
+    {
+        #region ICustomReportItem Members
+
+        bool ICustomReportItem.IsDataRegion()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICustomReportItem.DrawImage(System.Drawing.Bitmap bm)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICustomReportItem.DrawDesignerImage(System.Drawing.Bitmap bm)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICustomReportItem.SetProperties(IDictionary<string, object> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        object ICustomReportItem.GetPropertiesInstance(System.Xml.XmlNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICustomReportItem.SetPropertiesInstance(System.Xml.XmlNode node, object inst)
+        {
+            throw new NotImplementedException();
+        }
+
+        string ICustomReportItem.GetCustomReportItemXml()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        void IDisposable.Dispose()
+        {
+            return;
+        }
+
+        #endregion
+    }
+}
