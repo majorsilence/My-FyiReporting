fa65fac4639c0a8b80de9a90ec78971c1a8627c0
Peter Gill <peter@majorsilence.com>
2012-05-05 09:17:18 -0230

    Increase version number for next release.

--- LibRdlWpfViewer/Properties/AssemblyInfo.cs
+++ LibRdlWpfViewer/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LibRdlWpfViewer")]
+[assembly: AssemblyTitle("RDL Data WPF Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibRdlWpfViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("RDL Project")]
+[assembly: AssemblyCopyright("Copyright © 2012 Peter Gill")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.5.*")]
