5724bbb3ba71ec685f2972c41a99bc0455832083
Peter Gill <peter@majorsilence.com>
2012-08-26 11:33:51 -0230

    Remove login/logout from main menu.

--- ReportServer/Site.Master.designer.cs
+++ ReportServer/Site.Master.designer.cs
@@ -40,15 +40,6 @@ namespace ReportServer {
         protected global::System.Web.UI.WebControls.Literal LiteralLogin;
         
         /// <summary>
-        /// LiteralMenu control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal LiteralMenu;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
