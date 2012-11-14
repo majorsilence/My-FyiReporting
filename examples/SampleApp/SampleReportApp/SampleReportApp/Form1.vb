a2df2fa26ad0726d0fa4ec50d5a3b50baacbc16c
Peter Gill <peter@majorsilence.com>
2012-11-14 18:55:07 -0330

    Add parameter to connection string example.

--- examples/SampleApp/SampleReportApp/SampleReportApp/Form1.vb
+++ examples/SampleApp/SampleReportApp/SampleReportApp/Form1.vb
@@ -14,7 +14,7 @@
 
     Private Sub ButtonReloadReport_Click(sender As Object, e As EventArgs) Handles ButtonReloadReport.Click
         RdlViewer1.SourceFile = New Uri(file)
-        RdlViewer1.Parameters &= "ConnectionString=" & TextBox1.Text
+        RdlViewer1.Parameters = "ConnectionString=" & TextBox1.Text
         RdlViewer1.Rebuild()
     End Sub
 
