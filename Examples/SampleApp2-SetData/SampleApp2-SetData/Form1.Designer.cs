85c6f38e9af59390ca191043677b417f452ea6ea
Peter Gill <peter@majorsilence.com>
2012-11-25 18:10:51 -0330

    Sample project demonstrating setting data in code.

---
+++ Examples/SampleApp2-SetData/SampleApp2-SetData/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SampleApp2_SetData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdlViewer1 = new fyiReporting.RdlViewer.RdlViewer();
+            this.ButtonReloadReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rdlViewer1
+            // 
+            this.rdlViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdlViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rdlViewer1.Folder = null;
+            this.rdlViewer1.HighlightAll = false;
+            this.rdlViewer1.HighlightAllColor = System.Drawing.Color.Fuchsia;
+            this.rdlViewer1.HighlightCaseSensitive = false;
+            this.rdlViewer1.HighlightItemColor = System.Drawing.Color.Aqua;
+            this.rdlViewer1.HighlightPageItem = null;
+            this.rdlViewer1.HighlightText = null;
+            this.rdlViewer1.Location = new System.Drawing.Point(40, 69);
+            this.rdlViewer1.Name = "rdlViewer1";
+            this.rdlViewer1.PageCurrent = 1;
+            this.rdlViewer1.Parameters = null;
+            this.rdlViewer1.ReportName = null;
+            this.rdlViewer1.ScrollMode = fyiReporting.RdlViewer.ScrollModeEnum.Continuous;
+            this.rdlViewer1.SelectTool = false;
+            this.rdlViewer1.ShowFindPanel = false;
+            this.rdlViewer1.ShowParameterPanel = true;
+            this.rdlViewer1.ShowWaitDialog = true;
+            this.rdlViewer1.Size = new System.Drawing.Size(731, 381);
+            this.rdlViewer1.SourceFile = null;
+            this.rdlViewer1.SourceRdl = null;
+            this.rdlViewer1.TabIndex = 0;
+            this.rdlViewer1.UseTrueMargins = true;
+            this.rdlViewer1.Zoom = 0.8775232F;
+            this.rdlViewer1.ZoomMode = fyiReporting.RdlViewer.ZoomEnum.FitWidth;
+            // 
+            // ButtonReloadReport
+            // 
+            this.ButtonReloadReport.Location = new System.Drawing.Point(64, 31);
+            this.ButtonReloadReport.Name = "ButtonReloadReport";
+            this.ButtonReloadReport.Size = new System.Drawing.Size(141, 23);
+            this.ButtonReloadReport.TabIndex = 5;
+            this.ButtonReloadReport.Text = "Reload Report";
+            this.ButtonReloadReport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 462);
+            this.Controls.Add(this.ButtonReloadReport);
+            this.Controls.Add(this.rdlViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private fyiReporting.RdlViewer.RdlViewer rdlViewer1;
+        internal System.Windows.Forms.Button ButtonReloadReport;
+    }
+}
+
---
+++ Examples/SampleApp2-SetData/SampleApp2-SetData/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+
+namespace SampleApp2_SetData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // TODO: You must change this connection string to match where your database is
+            string connectionString = @"Data Source=C:\Users\Peter\Projects\My-FyiReporting\Examples\northwindEF.db;Version=3;Pooling=True;Max Pool Size=100;";
+            SQLiteConnection cn = new SQLiteConnection(connectionString);
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT CategoryID, CategoryName, Description FROM Categories;";
+            cmd.Connection = cn;
+            DataTable dt = GetTable(cmd);
+
+
+            string filepath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory() , "SampleApp2-TestReport.rdl");
+            rdlViewer1.SourceFile = new Uri(filepath);
+            rdlViewer1.Report.DataSets["Data"].SetData(dt);
+            rdlViewer1.Rebuild();
+        }
+
+
+        public DataTable GetTable(SQLiteCommand cmd)
+        {
+            System.Data.ConnectionState original = cmd.Connection.State;
+            if (cmd.Connection.State == ConnectionState.Closed)
+            {
+                cmd.Connection.Open();
+            }
+
+            DataTable dt = new DataTable();
+            SQLiteDataReader dr;
+
+            dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            dr.Close();
+            dr.Dispose();
+
+            if (original == ConnectionState.Closed)
+            {
+                cmd.Connection.Clone();
+            }
+
+            return dt;
+        }
+
+    }
+}
