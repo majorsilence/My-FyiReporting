cfbb58da8ece2c5ff483443e7f6c2f9635c1afbb
Peter <peter@peter-VirtualBox.(none)>
2013-04-26 20:45:07 -0230

    Script to create bundled RdlCmd app that can be used on a linux server that does not have mono installed.
    When building you should create a virtual machine similar to
    your server but with mono.  Build the bundle and copy
    it to your server.
    
    For database support other then Npgsql.dll add the dll as a
    dep after the --deps option.

---
+++ Release-Builds/BundleRdlCmd.sh
@@ -0,0 +1,12 @@
+cd ../RdlCmd/bin/Release/
+
+xbuild "RdlCmd.sln" /toolsversion:4.0 /p:Configuration="Release";Platform="AnyCPU" //m:4
+
+mkbundle -o RdlCmd --deps RdlCmd.exe RdlEngine.dll itextsharp.dll Mono.Security.dll Npgsql.dll ICSharpCode.SharpZipLib.dll --static
+
+cd ../../../Release-Builds
+
+rm -rf ./linux-rdlcmd-bundle
+mkdir ./linux-rdlcmd-bundle
+
+cp ../RdlCmd/bin/Release/RdlCmd ./linux-rdlcmd-bundle/RdlCmd
