f14ddfa273597e8ab925466c90e6e6a396a025d7
Peter Gill <peter@majorsilence.com>
2013-04-28 20:43:08 -0230

    #42 anchor example user control

--- Examples/SampleDesignerControl/SampleDesignerControl/Form1.Designer.cs
+++ Examples/SampleDesignerControl/SampleDesignerControl/Form1.Designer.cs
@@ -33,10 +33,22 @@
             // 
             // rdlUserControl1
             // 
+            this.rdlUserControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdlUserControl1.CurrentInsert = null;
             this.rdlUserControl1.Location = new System.Drawing.Point(29, 24);
+            this.rdlUserControl1.Modified = false;
             this.rdlUserControl1.Name = "rdlUserControl1";
+            this.rdlUserControl1.SelectionTool = false;
             this.rdlUserControl1.Size = new System.Drawing.Size(998, 469);
+            this.rdlUserControl1.SourceFile = null;
+            this.rdlUserControl1.SourceRdl = "<Report>\r\n  <Body>\r\n    <Height>0pt</Height>\r\n  </Body>\r\n  <PageHeader>\r\n    <Hei" +
+    "ght>0pt</Height>\r\n  </PageHeader>\r\n  <PageFooter>\r\n    <Height>0pt</Height>\r\n  <" +
+    "/PageFooter>\r\n</Report>";
             this.rdlUserControl1.TabIndex = 0;
+            this.rdlUserControl1.Zoom = 1F;
+            this.rdlUserControl1.ZoomMode = fyiReporting.RdlViewer.ZoomEnum.UseZoom;
             // 
             // Form1
             // 
@@ -46,6 +58,7 @@
             this.Controls.Add(this.rdlUserControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Examples/SampleDesignerControl/SampleDesignerControl/Form1.cs
+++ Examples/SampleDesignerControl/SampleDesignerControl/Form1.cs
@@ -16,5 +16,10 @@ namespace SampleApp2_SetData
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
