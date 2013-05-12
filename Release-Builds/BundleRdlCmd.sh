78b67f5c7e2ca6cd31b18721706762c750d10187
Peter <peter@peter-VirtualBox.(none)>
2013-05-12 14:04:04 -0230

    #45 add ubuntu 12.04 build support to bundle RdlCmd

--- Release-Builds/BundleRdlCmd.sh
+++ Release-Builds/BundleRdlCmd.sh
@@ -6,7 +6,7 @@ cd ../RdlCmd
 
 rm ./bin/Release/RdlCmd.exe
 # RdlCmd should still build and work with 3.5 and older versions of linux only suport 3.5 and not 4.0
-xbuild "RdlCmd-3.5-LinuxSupport.sln" /toolsversion:3.5 /p:Configuration="Release";Platform="AnyCPU" # /m:4
+xbuild "RdlCmd.sln" /toolsversion:4.0 /p:Configuration="Release";Platform="AnyCPU" # /m:4
 
 cd ./bin/Release/
 
@@ -14,12 +14,17 @@ rm ./RdlCmd.c
 rm ./RdlCmd.o
 rm ./RdlCmd
 
+# find the command for a specific distribution by watching terminal output
+#mkbundle -o RdlCmd --deps RdlCmd.exe RdlEngine.dll itextsharp.dll Mono.Security.dll Npgsql.dll ICSharpCode.SharpZipLib.dll --static
+
 # Support building with static glibc
 mkbundle -c -o RdlCmd.c -oo RdlCmd.o --deps RdlCmd.exe RdlEngine.dll itextsharp.dll Mono.Security.dll Npgsql.dll ICSharpCode.SharpZipLib.dll --static
 
-gcc -o RdlCmd -Wall `pkg-config --cflags mono` RdlCmd.c  `pkg-config --libs-only-L mono` -Wl,-Bstatic -lmono -Wl,-Bstatic `pkg-config --libs-only-l mono | sed -e "s/\-lmono //"` -static-libgcc RdlCmd.o
+# Debian 6
+# gcc -o RdlCmd -Wall `pkg-config --cflags mono` RdlCmd.c  `pkg-config --libs-only-L mono` -Wl,-Bstatic -lmono -Wl,-Bstatic `pkg-config --libs-only-l mono | sed -e "s/\-lmono //"` -static-libgcc RdlCmd.o
 
-
+# Ubuntu 12.04
+gcc -o RdlCmd -Wall `pkg-config --cflags mono-2` RdlCmd.c  `pkg-config --libs-only-L mono-2` -Wl,-Bstatic -lmono-2.0 -Wl,-Bstatic `pkg-config --libs-only-l mono-2 | sed -e "s/\-lmono-2.0 //"` -static-libgcc RdlCmd.o
 
 
 cd ../../../Release-Builds
