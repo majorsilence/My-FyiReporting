afa8bc249dad34d1783bce0c68bfa2dbe38162cc
Peter Gill <peter@majorsilence.com>
2013-12-12 22:18:01 -0330

    #69 fix path to bundled bin folder

--- Release-Builds/BundleRdlCmd.sh
+++ Release-Builds/BundleRdlCmd.sh
@@ -4,11 +4,11 @@
 
 cd ../RdlCmd
 
-rm ./bin/Release/RdlCmd.exe
+rm ./bin/BundleLinux/RdlCmd.exe
 # RdlCmd should still build and work with 3.5 and older versions of linux only suport 3.5 and not 4.0
 xbuild "RdlCmd.sln" /toolsversion:4.0 /p:Configuration="BundleLinux";Platform="AnyCPU" # /m:4
 
-cd ./bin/Release/
+cd ./bin/BundleLinux/
 
 rm ./RdlCmd.c
 rm ./RdlCmd.o
@@ -18,7 +18,7 @@ rm ./RdlCmd
 #mkbundle -o RdlCmd --deps RdlCmd.exe RdlEngine.dll itextsharp.dll Mono.Security.dll Npgsql.dll ICSharpCode.SharpZipLib.dll --static
 
 # Support building with static glibc
-mkbundle -c -o RdlCmd.c -oo RdlCmd.o --deps RdlCmd.exe RdlEngine.dll RdlCri.dll DataProviders.dll itextsharp.dll Mono.Security.dll Npgsql.dll ICSharpCode.SharpZipLib.dll --static
+mkbundle -c -o RdlCmd.c -oo RdlCmd.o --deps RdlCmd.exe RdlEngine.dll RdlCri.dll DataProviders.dll itextsharp.dll Mono.Security.dll Npgsql.dll ICSharpCode.SharpZipLib.dll Mono.Data.Sqlite.dll --static
 
 # Debian 6
 # gcc -o RdlCmd -Wall `pkg-config --cflags mono` RdlCmd.c  `pkg-config --libs-only-L mono` -Wl,-Bstatic -lmono -Wl,-Bstatic `pkg-config --libs-only-l mono | sed -e "s/\-lmono //"` -static-libgcc RdlCmd.o
@@ -32,6 +32,6 @@ cd ../../../Release-Builds
 rm -rf ./linux-rdlcmd-bundle
 mkdir ./linux-rdlcmd-bundle
 
-cp ../RdlCmd/bin/Release/RdlCmd ./linux-rdlcmd-bundle/RdlCmd
-cp ../RdlCmd/bin/Release/RdlEngineConfig.Linux.xml ./linux-rdlcmd-bundle/RdlEngineConfig.Linux.xml
+cp ../RdlCmd/bin/BundleLinux/RdlCmd ./linux-rdlcmd-bundle/RdlCmd
+cp ../RdlCmd/bin/BundleLinux/RdlEngineConfig.Linux.xml ./linux-rdlcmd-bundle/RdlEngineConfig.Linux.xml
 
