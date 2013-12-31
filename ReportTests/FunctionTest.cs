491e1fa8ac22c3241a45bde950588af595073a77
Peter Gill <peter@majorsilence.com>
2013-12-31 11:32:17 -0330

    New report and test class to test expression functions.

---
+++ ReportTests/FunctionTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportTests
+{
+    public class FunctionTest
+    {
+        [Test()]
+        public void Test1()
+        {
+
+            var conn = new fyiReporting.Data.XmlConnection("RdlEngineconfig.Linux.xml");
+            Assert.True(conn.Database == null);
+
+        }
+    }
+}
