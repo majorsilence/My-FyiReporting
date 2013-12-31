0c3564107b5bd49f79326c6b7dd8914f31902042
Peter Gill <peter@majorsilence.com>
2013-12-31 19:56:19 -0330

    Add unit test base report.

---
+++ ReportTests/DatabaseInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportTests
+{
+    public class DatabaseInfo
+    {
+
+        public static string Connection
+        {
+            get
+            {
+                string cwd = System.Environment.CurrentDirectory;
+                string db = System.IO.Path.Combine(cwd, "northwindEF.db");
+
+                string cn = String.Format("Data Source={0};Version=3;Pooling=True;Max Pool Size=100;", db);
+                return cn;
+            }
+        }
+
+    }
+}
---
+++ ReportTests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportTests
+{
+    public class Program
+    {
+         /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var test = new FunctionTest();
+            test.Test1();
+        }
+    }
+}
