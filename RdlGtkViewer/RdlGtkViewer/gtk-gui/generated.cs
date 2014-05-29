b03a7031040586aedc0ee50937ec24a071a12220
Andrey Gankov <gav@qsolution.ru>
2014-05-29 23:42:01 +0400

    [GtkViewer] PDF icon as resurce.

--- RdlGtkViewer/RdlGtkViewer/gtk-gui/generated.cs
+++ RdlGtkViewer/RdlGtkViewer/gtk-gui/generated.cs
@@ -5,24 +5,24 @@ namespace Stetic
 	internal class Gui
 	{
 		private static bool initialized;
-		
+
 		internal static void Initialize (Gtk.Widget iconRenderer)
 		{
 			if ((Stetic.Gui.initialized == false)) {
 				Stetic.Gui.initialized = true;
 				global::Gtk.IconFactory w1 = new global::Gtk.IconFactory ();
-				global::Gtk.IconSet w2 = new global::Gtk.IconSet (new global::Gdk.Pixbuf (global::System.IO.Path.Combine (global::System.AppDomain.CurrentDomain.BaseDirectory, "./Resources/Icons/Pdf_16x16.png")));
+				global::Gtk.IconSet w2 = new global::Gtk.IconSet (global::Gdk.Pixbuf.LoadFromResource ("fyiReporting.RdlGtkViewer.Resources.Icons.Pdf_16x16.png"));
 				w1.Add ("PdfIcon", w2);
 				w1.AddDefault ();
 			}
 		}
 	}
-	
+
 	internal class BinContainer
 	{
 		private Gtk.Widget child;
 		private Gtk.UIManager uimanager;
-		
+
 		public static BinContainer Attach (Gtk.Bin bin)
 		{
 			BinContainer bc = new BinContainer ();
@@ -31,32 +31,32 @@ namespace Stetic
 			bin.Added += new Gtk.AddedHandler (bc.OnAdded);
 			return bc;
 		}
-		
+
 		private void OnSizeRequested (object sender, Gtk.SizeRequestedArgs args)
 		{
 			if ((this.child != null)) {
 				args.Requisition = this.child.SizeRequest ();
 			}
 		}
-		
+
 		private void OnSizeAllocated (object sender, Gtk.SizeAllocatedArgs args)
 		{
 			if ((this.child != null)) {
 				this.child.Allocation = args.Allocation;
 			}
 		}
-		
+
 		private void OnAdded (object sender, Gtk.AddedArgs args)
 		{
 			this.child = args.Widget;
 		}
-		
+
 		public void SetUiManager (Gtk.UIManager uim)
 		{
 			this.uimanager = uim;
 			this.child.Realized += new System.EventHandler (this.OnRealized);
 		}
-		
+
 		private void OnRealized (object sender, System.EventArgs args)
 		{
 			if ((this.uimanager != null)) {
@@ -69,14 +69,14 @@ namespace Stetic
 			}
 		}
 	}
-	
+
 	internal class ActionGroups
 	{
 		public static Gtk.ActionGroup GetActionGroup (System.Type type)
 		{
 			return Stetic.ActionGroups.GetActionGroup (type.FullName);
 		}
-		
+
 		public static Gtk.ActionGroup GetActionGroup (string name)
 		{
 			return null;
