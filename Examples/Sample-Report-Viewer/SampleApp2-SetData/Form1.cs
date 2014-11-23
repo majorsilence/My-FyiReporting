8ab2eb0aad7c8e6c560873e9917d74ac4b389cae
Peter Gill <peter@majorsilence.com>
2014-11-23 12:00:21 -0330

    Switch to toolbar control is report viewer example

--- Examples/Sample-Report-Viewer/SampleApp2-SetData/Form1.cs
+++ Examples/Sample-Report-Viewer/SampleApp2-SetData/Form1.cs
@@ -15,9 +15,7 @@ namespace SampleApp2_SetData
     public class Form1 : Form
     {
         private fyiReporting.RdlViewer.RdlViewer rdlViewer1;
-        private ToolStrip bar;
-        private ToolStripTextBox currentPage = new ToolStripTextBox("Current Page");
-        private ToolStripLabel pageCount = new ToolStripLabel("Page Count");
+        private fyiReporting.RdlViewer.ViewerToolstrip reportStrip;
 
         public Form1()
         {
@@ -31,174 +29,10 @@ namespace SampleApp2_SetData
             cmd.CommandText = "SELECT CategoryID, CategoryName, Description FROM Categories;";
             cmd.Connection = cn;
       
- 
-        }
-
-        private void OpenClicked(object sender, System.EventArgs e)
-        {
-            var dlg = new OpenFileDialog();
-            var result = dlg.ShowDialog();
-            if (result != DialogResult.OK)
-            {
-                return;
-            }
-
-            rdlViewer1.SourceFile = new Uri(dlg.FileName);
-            rdlViewer1.Rebuild();
-
-            currentPage.Text = rdlViewer1.PageCurrent.ToString();
-            pageCount.Text = rdlViewer1.PageCount.ToString();
-        }
-
-        private void PrintClicked(object sender, System.EventArgs e)
-        {
-
-            PrintDocument pd = new PrintDocument();
-            pd.DocumentName = rdlViewer1.SourceFile.LocalPath;
-            pd.PrinterSettings.FromPage = 1;
-            pd.PrinterSettings.ToPage = rdlViewer1.PageCount;
-            pd.PrinterSettings.MaximumPage = rdlViewer1.PageCount;
-            pd.PrinterSettings.MinimumPage = 1;
-            pd.DefaultPageSettings.Landscape = rdlViewer1.PageWidth > rdlViewer1.PageHeight ? true : false;
-            using (PrintDialog dlg = new PrintDialog())
-            {
-                dlg.Document = pd;
-                dlg.AllowSelection = true;
-                dlg.AllowSomePages = true;
-                if (dlg.ShowDialog() == DialogResult.OK)
-                {
-                    rdlViewer1.Print(pd);
-                }
-            }
-     
-        }
-
-        private void SaveAsClicked(object sender, System.EventArgs e)
-        {
-            var dlg = new SaveFileDialog();
-            dlg.Filter = "PDF files|*.pdf|XML files|*.xml|HTML files|*.html|CSV files|*.csv|RTF files|*.rtf|TIF files|*.tif|Excel files|*.xlsx|MHT files|*.mht";
-            dlg.FileName = ".pdf";
-            var result = dlg.ShowDialog();
-            if (result != DialogResult.OK)
-            {
-                return;
-            }
-            // save the report in a rendered format
-            string ext = null;
-            int i = dlg.FileName.LastIndexOf('.');
-            if (i < 1)
-            {
-                ext = "";
-            }
-            else
-            {
-                ext = dlg.FileName.Substring(i + 1).ToLower();
-            }
-            fyiReporting.RDL.OutputPresentationType type = fyiReporting.RDL.OutputPresentationType.Internal;
-            switch (ext)
-            {
-                case "pdf":
-                    type = fyiReporting.RDL.OutputPresentationType.PDF;
-                    break;
-                case "xml":
-                    type = fyiReporting.RDL.OutputPresentationType.XML;
-                    break;
-                case "html":
-                    type = fyiReporting.RDL.OutputPresentationType.HTML;
-                    break;
-                case "htm":
-                    type = fyiReporting.RDL.OutputPresentationType.HTML;
-                    break;
-                case "csv":
-                    type = fyiReporting.RDL.OutputPresentationType.CSV;
-                    break;
-                case "rtf":
-                    type = fyiReporting.RDL.OutputPresentationType.RTF;
-                    break;
-                case "mht":
-                    type = fyiReporting.RDL.OutputPresentationType.MHTML;
-                    break;
-                case "mhtml":
-                    type = fyiReporting.RDL.OutputPresentationType.MHTML;
-                    break;
-                case "xlsx":
-                    type = fyiReporting.RDL.OutputPresentationType.Excel;
-                    break;
-                case "tif":
-                    type = fyiReporting.RDL.OutputPresentationType.TIF;
-                    break;
-                case "tiff":
-                    type = fyiReporting.RDL.OutputPresentationType.TIF;
-                    break;
-                default:
-                    MessageBox.Show(String.Format("{0} is not a valid file type. File extension must be PDF, XML, HTML, CSV, MHT, RTF, TIF, XLSX.", dlg.FileName),
-                        "Invalid File Type", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    break;
-            }
-
-            rdlViewer1.SaveAs(dlg.FileName, type);
-        }
-
-        private void FirstPageClicked(object sender, System.EventArgs e)
-        {
-            rdlViewer1.PageCurrent = 1;
-        }
-
-        private void PreviousPageClicked(object sender, System.EventArgs e)
-        {
-
-            if (rdlViewer1.PageCurrent == 1)
-            {
-                return;
-            }
-
-            rdlViewer1.PageCurrent -= 1;
 
         }
 
-        private void NextPageClicked(object sender, System.EventArgs e)
-        {
-
-            if (rdlViewer1.PageCurrent == rdlViewer1.PageCount)
-            {
-                return;
-            }
-            rdlViewer1.PageCurrent += 1;
-
-        }
-
-        private void LastPageClicked(object sender, System.EventArgs e)
-        {
-            rdlViewer1.PageCurrent = rdlViewer1.PageCount;
-        }
-
-        private void ZoomInClicked(object sender, System.EventArgs e)
-        {
-            rdlViewer1.Zoom += 0.5f;
-        }
-
-        private void ZoomOutClicked(object sender, System.EventArgs e)
-        {
-            rdlViewer1.Zoom -= 0.5f;
-        }
-
-        private void InitializeToolBar()
-        {
-            this.bar = new ToolStrip();
-            this.Controls.Add(bar);
-
-            bar.Items.Add(new ToolStripButton("Open", null, OpenClicked));
-            bar.Items.Add(new ToolStripButton("Save As", null, SaveAsClicked));
-            bar.Items.Add(new ToolStripButton("Print", null, PrintClicked));
-            bar.Items.Add(new ToolStripButton("First Page", null, FirstPageClicked));
-            bar.Items.Add(new ToolStripButton("Previous Page", null, PreviousPageClicked));
-            bar.Items.Add(new ToolStripButton("Next Page", null, NextPageClicked));
-            bar.Items.Add(new ToolStripButton("Last Page", null, LastPageClicked));
-            bar.Items.Add(this.currentPage);
-            bar.Items.Add(this.pageCount);
-            bar.Items.Add(new ToolStripButton("Zoom In", null, ZoomInClicked));
-            bar.Items.Add(new ToolStripButton("Zoom Out", null, ZoomOutClicked));
-        }
+       
 
         private void InitializeViewer()
         {
@@ -212,20 +46,19 @@ namespace SampleApp2_SetData
 
             this.rdlViewer1.Size = new System.Drawing.Size(731, 381);
 
-            this.rdlViewer1.PageNavigation += HandlePageNavigation;
 
         }
 
-        void HandlePageNavigation(object sender, fyiReporting.RdlViewer.PageNavigationEventArgs e)
-        {
-            currentPage.Text = e.NewPage.ToString();
-        }
-
+       
 
         private void InitializeComponent()
         {
-            InitializeToolBar();
             InitializeViewer();
+
+            reportStrip = new fyiReporting.RdlViewer.ViewerToolstrip(rdlViewer1);
+            //reportStrip.Location = new Point(0, 0);
+            this.Controls.Add(reportStrip);
+       
 
          
             this.SuspendLayout();
