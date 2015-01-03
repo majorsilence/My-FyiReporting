cbec63464224d5bf291b6537db0f7b4cf23c69e3
Peter Gill <peter@majorsilence.com>
2015-01-02 21:26:11 -0330

    Increase version numbers to prepare for next release.

--- LibRdlWpfViewer/Properties/AssemblyInfo.cs
+++ LibRdlWpfViewer/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyVersion("4.7.*")]
+// [assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyVersion("4.8.*")]
--- RdlMapFile/Properties/AssemblyInfo.cs
+++ RdlMapFile/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.7.*")]
+[assembly: AssemblyVersion("4.8.*")]
