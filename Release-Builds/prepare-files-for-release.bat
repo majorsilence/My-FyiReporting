977acc96f281946bb965823d01c1da163430aea7
Peter Gill <peter@majorsilence.com>
2015-01-02 21:23:55 -0330

    Fix script to properly set nuspec versions

--- Release-Builds/prepare-files-for-release.bat
+++ Release-Builds/prepare-files-for-release.bat
@@ -3,6 +3,7 @@ REM requires you have gow 0.8.0 or newer installed (see https://github.com/bmatz
 
 REM Increment minor number by 1
 REM See http://www.codeproject.com/Articles/31236/How-To-Update-Assembly-Version-Number-Automaticall
+
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\DataProviders\AssemblyInfo.cs"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\LibRdlCrossPlatformViewer\Properties\AssemblyInfo.cs"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\LibRdlWpfViewer\Properties\AssemblyInfo.cs"
@@ -21,16 +22,18 @@ REM See http://www.codeproject.com/Articles/31236/How-To-Update-Assembly-Version
 
 REM retrieve new version number
 REM See http://stackoverflow.com/questions/4044579/how-to-get-output-of-a-for-loop-into-a-variable-in-a-batch-file
-cd "%CD%\..\RdlEngine\AssemblyInfo.cs"
-for /f "tokens=2 delims=()" %%a in ('findstr AssemblyVersion AssemblyInfo.vb') do set Version=%%~a
+cd "%CD%\..\RdlEngine"
+for /f "tokens=2 delims=()" %%a in ('findstr AssemblyVersion AssemblyInfo.cs') do set Version=%%~a
 
+set Version=%Version:.*=%
+echo %Version%
 cd ..
 cd Release-Builds
 
 REM Update NUGET spec files
-sed.exe -i "5s/.*/ <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Asp\MajorsilenceReporting-Asp.nuspec"
-sed.exe -i "5s/.*/ <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Core\MajorsilenceReporting-Core.nuspec"
-sed.exe -i "5s/.*/ <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-XwtViewer\MajorsilenceReporting-XwtViewer.nuspec"
-sed.exe -i "5s/.*/ <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Viewer\MajorsilenceReporting-Viewer.nuspec"
+sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Asp\MajorsilenceReporting-Asp.nuspec"
+sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Core\MajorsilenceReporting-Core.nuspec"
+sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-XwtViewer\MajorsilenceReporting-XwtViewer.nuspec"
+sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Viewer\MajorsilenceReporting-Viewer.nuspec"
 
 del "sed*"
