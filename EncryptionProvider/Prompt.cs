958b940f5fdca383b9bc4554e53b1a42c8eca40b
grezza123 <grezza123@hotmail.com>
2016-02-12 15:15:49 +0000

    -- Correction of the issue with the pull request -- also added loading and saving of .encrypted files in the viewer/reader

--- EncryptionProvider/Prompt.cs
+++ EncryptionProvider/Prompt.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -16,7 +17,7 @@ namespace EncryptionProvider
             prompt.Height = 200;
             prompt.FormBorderStyle = FormBorderStyle.FixedDialog;
             prompt.Text = caption;
-            prompt.StartPosition = FormStartPosition.CenterScreen;
+            
             Label textLabel = new Label() {Left = 50, Top = 20, Width = 400, Height = 110, Text = text};
             TextBox textBox = new TextBox() {Left = 50, Top = 100, Width = 400};
             Button confirmation = new Button()
@@ -32,7 +33,25 @@ namespace EncryptionProvider
             prompt.Controls.Add(textLabel);
             prompt.AcceptButton = confirmation;
             prompt.ShowDialog();
+            prompt.TopMost = true;
+
+
+            Screen screen = Screen.FromControl(prompt);
+
+            Rectangle workingArea = screen.WorkingArea;
+            prompt.Location = new Point()
+            {
+                X = Math.Max(workingArea.X, workingArea.X + (workingArea.Width - prompt.Width) / 2),
+                Y = Math.Max(workingArea.Y, workingArea.Y + (workingArea.Height - prompt.Height) / 3)
+            };
+            prompt.StartPosition = FormStartPosition.Manual;
+
+
+
             return textBox.Text;
         }
+
+       
+            
     }
 }
