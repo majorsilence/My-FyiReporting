1ccfe18712a751584d58ea88a1cbe51e6b2c44ca
Andrey Gankov <gav@qsolution.ru>
2016-06-17 14:19:33 +0300

    Replace Excel 2003 to Excel 2007.

--- RdlGtkViewer/RdlGtkViewer/ReportViewer.cs
+++ RdlGtkViewer/RdlGtkViewer/ReportViewer.cs
@@ -435,8 +435,8 @@ namespace fyiReporting.RdlGtkViewer
             Gtk.FileFilter asphtmlFilter = new Gtk.FileFilter();
             asphtmlFilter.Name = "ASP HTML";
 			
-            Gtk.FileFilter excel2003 = new Gtk.FileFilter();
-            excel2003.Name = "Excel 2003";
+			Gtk.FileFilter excel2007 = new Gtk.FileFilter();
+            excel2007.Name = "Excel 2007";
 			
             Gtk.FileFilter htmlFilter = new Gtk.FileFilter();
             htmlFilter.Name = "HTML";
@@ -453,7 +453,7 @@ namespace fyiReporting.RdlGtkViewer
             fc.AddFilter(pdfFilter);
             fc.AddFilter(csvFilter);
             fc.AddFilter(asphtmlFilter);
-            fc.AddFilter(excel2003);
+            fc.AddFilter(excel2007);
             fc.AddFilter(htmlFilter);
             fc.AddFilter(mhtmlFilter);
             fc.AddFilter(xmlFilter);
@@ -491,7 +491,7 @@ namespace fyiReporting.RdlGtkViewer
                             filename = filename + ".asphtml";
                         }
                     }
-                    else if (fc.Filter.Name == "Excel 2003")
+                    else if (fc.Filter.Name == "Excel 2007")
                     {
                         exportType = OutputPresentationType.Excel;
                         if (filename.ToLower().Trim().EndsWith(".xlsx") == false)
