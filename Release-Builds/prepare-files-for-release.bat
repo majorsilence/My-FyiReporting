49ecef92f4bb15317fb7e00c1d34d0c062f5e2cf
Peter Gill <peter@majorsilence.com>
2024-03-31 17:46:56 -0230

    use modern sdk style generate nuget packages

--- Release-Builds/prepare-files-for-release.bat
+++ Release-Builds/prepare-files-for-release.bat
@@ -1,40 +1,21 @@
 
-REM requires you have gow 0.8.0 or newer installed (see https://github.com/bmatzelle/gow).  Or version of sed with in place editing in your path
-
 REM Increment minor number by 1
 REM See http://www.codeproject.com/Articles/31236/How-To-Update-Assembly-Version-Number-Automaticall
+REM See https://github.com/TownSuite/AssemblyInfoUtil
 
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\DataProviders\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\LibRdlCrossPlatformViewer\Properties\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\LibRdlWpfViewer\Properties\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlAsp\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlCmd\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlCri\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlDesign\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlDesktop\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlEngine\AssemblyInfo.cs"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\DataProviders\DataProviders.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\LibRdlCrossPlatformViewer\LibRdlCrossPlatformViewer.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\LibRdlWpfViewer\LibRdlWpfViewer.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlAsp\RdlAsp.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlCmd\RdlCmd.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlCri\RdlCri.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlDesign\RdlDesign.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlDesktop\RdlDesktop.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlEngine\RdlEngine.csproj"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlGtkViewer\RdlGtkViewer\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlMapFile\Properties\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer\AssemblyInfo.cs"
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer\RdlReader\AssemblyInfo.cs"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlMapFile\RdlMapFile.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer\RdlViewer.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer\RdlReader\RdlViewer.csproj"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\ReportServer\Properties\AssemblyInfo.cs"
 
 
-REM retrieve new version number
-REM See http://stackoverflow.com/questions/4044579/how-to-get-output-of-a-for-loop-into-a-variable-in-a-batch-file
-cd "%CD%\..\RdlEngine"
-for /f "tokens=2 delims=()" %%a in ('findstr AssemblyVersion AssemblyInfo.cs') do set Version=%%~a
-
-set Version=%Version:.*=%
-echo %Version%
-cd ..
-cd Release-Builds
-
-REM Update NUGET spec files
-sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Asp\MajorsilenceReporting-Asp.nuspec"
-sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Core\MajorsilenceReporting-Core.nuspec"
-sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-XwtViewer\MajorsilenceReporting-XwtViewer.nuspec"
-sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-Viewer\MajorsilenceReporting-Viewer.nuspec"
-sed.exe -i "5s/.*/    <version>%Version%<\/version>/" "%CD%\nuget\MajorsilenceReporting-WpfViewer\MajorsilenceReporting-WpfViewer.nuspec"
-
-del "sed*"
--- build-release.ps1
+++ build-release.ps1
@@ -31,38 +31,27 @@ function delete_files([string]$path)
 
 function GetVersions([ref]$theVersion)
 {
-	$path = "$CURRENTPATH/../RdlEngine/AssemblyInfo.cs"
-	$pattern = '\[assembly: AssemblyVersion\("(.*)"\)\]'
-	(Get-Content $path) | ForEach-Object{
-		if($_ -match $pattern){
-			# We have found the matching line
-			$version = [string]$matches[1] 
-			$theVersion.value= $version -replace "\.\*", ""
-			
-			$_
-		} else {
-			# Output line as is
-			$_
-		}
-	} | Set-Content $path
+	$csprojPath = Join-Path $CURRENTPATH ".\RdlEngine\RdlEngine.csproj"
+	$xml = [xml](Get-Content $csprojPath)
+	$theVersion.Value = $xml.Project.PropertyGroup.Version
 }
 
-Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
+Get-ChildItem .\ -include bin,obj,build-output -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 
 $Version=""
 GetVersions([ref]$Version)
-Write-Host $Version
-dotnet restore "../MajorsilenceReporting.sln"
+Write-Host  $Version
+dotnet restore "./MajorsilenceReporting.sln"
 
 # ************* Begin net48 anycpu *********************************************
 
-& "$msbuildpath" "$CURRENTPATH\..\MajorsilenceReporting.sln" /verbosity:minimal /p:Configuration="Release" /property:Platform="Any CPU" /target:clean /target:rebuild
+& "$msbuildpath" "$CURRENTPATH\MajorsilenceReporting.sln" /verbosity:minimal /p:Configuration="Release" /property:Platform="Any CPU" /target:clean /target:rebuild
 
-$buildoutputpath_designer="$CURRENTPATH\build-output\majorsilence-reporting-designer-net48-anycpu"
-$buildoutputpath_desktop="$CURRENTPATH\build-output\majorsilence-reporting-desktop-net48-anycpu"
-$buildoutputpath_rdlcmd="$CURRENTPATH\build-output\majorsilence-reporting-rdlcmd-net48-anycpu"
-$buildoutputpath_viewer="$CURRENTPATH\build-output\majorsilence-reporting-viewer-net48-anycpu"
-$buildoutputpath_mapfile="$CURRENTPATH\build-output\majorsilence-reporting-mapfile-net48-anycpu"
+$buildoutputpath_designer="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-designer-net48-anycpu"
+$buildoutputpath_desktop="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-desktop-net48-anycpu"
+$buildoutputpath_rdlcmd="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-rdlcmd-net48-anycpu"
+$buildoutputpath_viewer="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-viewer-net48-anycpu"
+$buildoutputpath_mapfile="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-mapfile-net48-anycpu"
 Remove-Item "$buildoutputpath_designer" -Recurse -ErrorAction Ignore
 mkdir "$buildoutputpath_designer"
 Remove-Item "$buildoutputpath_desktop" -Recurse -ErrorAction Ignore
@@ -74,71 +63,79 @@ mkdir "$buildoutputpath_viewer"
 Remove-Item "$buildoutputpath_mapfile" -Recurse -ErrorAction Ignore
 mkdir "$buildoutputpath_mapfile"
 
-Copy-Item ..\RdlDesign\bin\Release\net48\ -Destination "$buildoutputpath_designer\" -Recurse
-Copy-Item ..\RdlDesktop\bin\Release\net48\ -Destination "$buildoutputpath_desktop\" -Recurse
-Copy-Item ..\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_rdlcmd\" -Recurse
-Copy-Item ..\RdlViewer\bin\Release\net48\ -Destination "$buildoutputpath_viewer\" -Recurse
-Copy-Item ..\RdlMapFile\bin\Release\net48\ -Destination "$buildoutputpath_mapfile\" -Recurse
+Copy-Item .\RdlDesign\bin\Release\net48\ -Destination "$buildoutputpath_designer\" -Recurse
+Copy-Item .\RdlDesktop\bin\Release\net48\ -Destination "$buildoutputpath_desktop\" -Recurse
+Copy-Item .\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_rdlcmd\" -Recurse
+Copy-Item .\RdlViewer\bin\Release\net48\ -Destination "$buildoutputpath_viewer\" -Recurse
+Copy-Item .\RdlMapFile\bin\Release\net48\ -Destination "$buildoutputpath_mapfile\" -Recurse
 
+cd Release-Builds
 cd build-output	
 ..\7za.exe a $Version-majorsilence-reporting-designer-net48-anycpu.zip majorsilence-reporting-designer-net48-anycpu\
 ..\7za.exe a $Version-majorsilence-reporting-desktop-net48-anycpu.zip majorsilence-reporting-desktop-net48-anycpu\
 ..\7za.exe a $Version-majorsilence-reporting-rdlcmd-net48-anycpu.zip majorsilence-reporting-rdlcmd-net48-anycpu\
 ..\7za.exe a $Version-majorsilence-reporting-viewer-net48-anycpu.zip majorsilence-reporting-viewer-net48-anycpu\
 ..\7za.exe a $Version-majorsilence-reporting-mapfile-net48-anycpu.zip majorsilence-reporting-mapfile-net48-anycpu\
-cd ..
+cd "$CURRENTPATH"
 
 # ************* End x64 *********************************************
 
 
 # ************* Begin PHP *********************************************
-$buildoutputpath_php="$CURRENTPATH\build-output\majorsilence-reporting-php"
+$buildoutputpath_php="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-php"
 delete_files "$buildoutputpath_php"
 mkdir "$buildoutputpath_php"
 
-Copy-Item ..\RdlDesktop\bin\Release\net48\config.xml "$buildoutputpath_php\config.xml"
-Copy-Item ..\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_php\" -Recurse
+Copy-Item .\RdlDesktop\bin\Release\net48\config.xml "$buildoutputpath_php\config.xml"
+Copy-Item .\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_php\" -Recurse
 
-Copy-Item "..\LanguageWrappers\php\config.php" "$buildoutputpath_php\config.php"
-Copy-Item "..\LanguageWrappers\php\report.php" "$buildoutputpath_php\report.php"
+Copy-Item ".\LanguageWrappers\php\config.php" "$buildoutputpath_php\config.php"
+Copy-Item ".\LanguageWrappers\php\report.php" "$buildoutputpath_php\report.php"
 
+cd Release-Builds
 cd build-output	
 ..\7za.exe a $Version-majorsilence-reporting-build-php.zip majorsilence-reporting-php\
-cd ..
+cd "$CURRENTPATH"
 
 # ************* End PHP *********************************************
 
 
 
 # ************* Begin Python *********************************************
-$buildoutputpath_python="$CURRENTPATH\build-output\majorsilence-reporting-python"
+$buildoutputpath_python="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-python"
 delete_files "$buildoutputpath_python"
 mkdir "$buildoutputpath_python"
 
-Copy-Item ..\RdlDesktop\bin\Release\net48\config.xml "$buildoutputpath_python\config.xml"
-Copy-Item ..\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_python\" -Recurse
-Copy-Item "..\LanguageWrappers\python\config.py" "$buildoutputpath_python\config.py"
-Copy-Item "..\LanguageWrappers\python\report.py" "$buildoutputpath_python\report.py"
+Copy-Item .\RdlDesktop\bin\Release\net48\config.xml "$buildoutputpath_python\config.xml"
+Copy-Item .\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_python\" -Recurse
+Copy-Item ".\LanguageWrappers\python\config.py" "$buildoutputpath_python\config.py"
+Copy-Item ".\LanguageWrappers\python\report.py" "$buildoutputpath_python\report.py"
 
+cd Release-Builds
 cd build-output	
 ..\7za.exe a $Version-majorsilence-reporting-python.zip majorsilence-reporting-python\
-cd ..
+cd "$CURRENTPATH"
 # ************* End Python *********************************************
 
 
 # ************* Begin Ruby *********************************************
-$buildoutputpath_ruby="$CURRENTPATH\build-output\majorsilence-reporting-ruby"
+$buildoutputpath_ruby="$CURRENTPATH\Release-Builds\build-output\majorsilence-reporting-ruby"
 delete_files "$buildoutputpath_ruby"
 mkdir "$buildoutputpath_ruby"
 
-Copy-Item ..\RdlDesktop\bin\Release\net48\config.xml "$buildoutputpath_ruby\config.xml"
-Copy-Item ..\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_ruby\" -Recurse
+Copy-Item .\RdlDesktop\bin\Release\net48\config.xml "$buildoutputpath_ruby\config.xml"
+Copy-Item .\RdlCmd\bin\Release\net48\ -Destination "$buildoutputpath_ruby\" -Recurse
 
-Copy-Item "..\LanguageWrappers\ruby\config.rb" "$buildoutputpath_ruby\config.rb"
-Copy-Item "..\LanguageWrappers\ruby\report.rb" "$buildoutputpath_ruby\report.rb"
+Copy-Item ".\LanguageWrappers\ruby\config.rb" "$buildoutputpath_ruby\config.rb"
+Copy-Item ".\LanguageWrappers\ruby\report.rb" "$buildoutputpath_ruby\report.rb"
 
+cd Release-Builds
 cd build-output	
 ..\7za.exe a $Version-majorsilence-reporting-ruby.zip majorsilence-reporting-ruby\
-cd ..
+cd "$CURRENTPATH"
 
 # ************* End Ruby *********************************************
+
+
+# ************* Nuget ************************************************
+Get-ChildItem -Recurse -Exclude "$CURRENTPATH\Release-Builds\build-output" .\*.nupkg | Copy-Item -Destination "$CURRENTPATH\Release-Builds\build-output" -Force -ErrorAction SilentlyContinue
