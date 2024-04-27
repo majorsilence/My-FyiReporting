fb5660ddafe6edc77c947b8859601dfa42774522
Peter Gill <peter@majorsilence.com>
2024-04-27 19:52:47 -0230

    linux target net8.0

--- build-release-rdlcmd-linux.ps1
+++ build-release-rdlcmd-linux.ps1
@@ -23,8 +23,8 @@ function BuildRdlCmd{
 	rmf -rf $buildoutputpath_rdlcmd_linux
 	mkdir -p $buildoutputpath_rdlcmd_linux
 
-	dotnet publish RdlCmd -c Release -r linux-x64 -f net6.0 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-	Copy-Item ./RdlCmd/bin/Release/net6.0/linux-x64/publish -Destination "$buildoutputpath_rdlcmd_linux/" -Recurse
+	dotnet publish RdlCmd -c Release -r linux-x64 -f net8.0 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
+	Copy-Item ./RdlCmd/bin/Release/net8.0/linux-x64/publish -Destination "$buildoutputpath_rdlcmd_linux/" -Recurse
 
 	cd Release-Builds
 	cd build-output	
@@ -41,9 +41,11 @@ if ($arg -eq "build") {
     BuildRdlCmd
 } else {
 	Write-Output "Usage: ./build-release-linux.ps1 docker"
-	Write-Output "net6.0 for linux-x64 is the last version that System.Drawing can be enabled."
+	Write-Output "net8.0 using the mono System.Drawing.Common library from nuget package ZKWeb.System.Drawing"
+	Write-Output "requires libgdiplus be installed apt-get install -y libgdiplus"
+	Write-Output "Micrsoft has deprecated System.Drawing.Common in .net 6.0"
 	Write-Output "https://learn.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/6.0/system-drawing-common-windows-only"
-    docker run --memory 4gb --rm -v ${CURRENTPATH}:/src mcr.microsoft.com/dotnet/sdk:6.0 bash -c "cd /src && ./build-release-linux.ps1 build"
+    docker run --memory 4gb --rm -v ${CURRENTPATH}:/src mcr.microsoft.com/dotnet/sdk:8.0 bash -c "cd /src && ./build-release-linux.ps1 build"
 }
 
 		
