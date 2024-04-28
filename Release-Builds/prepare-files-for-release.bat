042b05a1659c4bc84da35d946902e7564e2c2781
Peter Gill <peter@majorsilence.com>
2024-04-28 19:39:27 -0230

    Version 4.17 in all projects
    GenerateAssemblyInfo, true
    Deterministic, true

--- Release-Builds/prepare-files-for-release.bat
+++ Release-Builds/prepare-files-for-release.bat
@@ -15,7 +15,12 @@ REM See https://github.com/TownSuite/AssemblyInfoUtil
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlGtkViewer\RdlGtkViewer\AssemblyInfo.cs"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlMapFile\RdlMapFile.csproj"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer\RdlViewer.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer.Tests\RdlViewer.Tests.csproj"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlViewer\RdlReader\RdlViewer.csproj"
 .\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\ReportServer\Properties\AssemblyInfo.cs"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlCreator\RdlCreator.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlCreator.Tests\RdlCreator.Tests.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlGtk3\RdlGtk3.csproj"
+.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\RdlGtk3Viewer\RdlGtk3Viewer.csproj"
 
 
