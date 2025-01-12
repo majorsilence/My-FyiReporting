41df97bf4ad6e9bce04ef046b32cc07c2d542162
Peter Gill <peter@majorsilence.com>
2025-01-12 12:50:03 -0330

    Refactor and enhance cryptographic implementations
    Refactored code for readability and maintainability. Replaced `RNGCryptoServiceProvider` with `RandomNumberGenerator.Fill` for generating random bytes. Switched from `PasswordDeriveBytes` to `Rfc2898DeriveBytes` for PBKDF2 with SHA-256. Updated from `RijndaelManaged` to `Aes` for AES encryption. Replaced `try-finally` blocks with `using` statements for resource disposal. Streamlined `Create` and `Retrieve` methods for better clarity and efficiency.

--- RdlEngine/Definition/DataSourceReference.cs
+++ RdlEngine/Definition/DataSourceReference.cs
@@ -29,131 +29,86 @@ using System.Text;
 
 namespace fyiReporting.RDL
 {
-	///<summary>
-	/// Utitlity class for reading and writing a DataSourceReference file
-	///</summary>
-	public sealed class DataSourceReference
-	{
-		const int IV_SIZE=16;
-		const int KEY_SIZE=32;
-		/// <summary>
-		/// Create the named file containing the encrypted input data
-		/// </summary>
-		/// <param name="filename">Output file name</param>
-		/// <param name="indata">Input data to place in file encrypted.</param>
-		/// <param name="pswd">Password phrase to use when encrypting</param>
-		static public void Create(string filename, string indata, string pswd) 
-		{
-			// create the salt
-			RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-			byte[] salt = new byte[IV_SIZE];
-			rng.GetBytes(salt);
+    ///<summary>
+    /// Utitlity class for reading and writing a DataSourceReference file
+    ///</summary>
+    public sealed class DataSourceReference
+    {
+        const int IV_SIZE = 16;
+        const int KEY_SIZE = 32;
+        /// <summary>
+        /// Create the named file containing the encrypted input data
+        /// </summary>
+        /// <param name="filename">Output file name</param>
+        /// <param name="indata">Input data to place in file encrypted.</param>
+        /// <param name="pswd">Password phrase to use when encrypting</param>
+        static public void Create(string filename, string indata, string pswd)
+        {
+            // create the salt
+            byte[] salt = new byte[IV_SIZE];
+            RandomNumberGenerator.Fill(salt);
 
-			// create the key from the password phrase
-			PasswordDeriveBytes pdb = new PasswordDeriveBytes(pswd, salt);
-			byte[] key = pdb.GetBytes(KEY_SIZE);
+            // create the key from the password phrase
+            var pdb = new Rfc2898DeriveBytes(pswd, salt, 10000, HashAlgorithmName.SHA256);
+            byte[] key = pdb.GetBytes(KEY_SIZE);
 
-			// Create an instance of the RijndaelManaged class
-			RijndaelManaged rm = new RijndaelManaged();
-			CryptoStream cs = null;
-			MemoryStream ms = null;
-			try
-			{
-				ms = new MemoryStream(1000);	// memory stream to put encrypted data to
-				cs = new CryptoStream(ms, rm.CreateEncryptor(key, salt), CryptoStreamMode.Write);
-				byte[] ta = Encoding.UTF8.GetBytes(indata);
-				cs.Write(ta, 0, ta.Length);
-			}
-			finally
-			{
-				if (cs != null)
-					cs.Close();
-				if (ms != null)
-					ms.Close();
-			}
+            // Create an instance of the Aes class
+            using (Aes aes = Aes.Create())
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(key, salt), CryptoStreamMode.Write))
+            {
+                byte[] ta = Encoding.UTF8.GetBytes(indata);
+                cs.Write(ta, 0, ta.Length);
+                cs.Close();
 
-			FileStream fs = null;
-			try
-			{
-				fs = File.OpenWrite(filename);
-				// Write the salt to the beginning of the file
-				fs.Write(salt, 0, salt.Length);
-				// Write the encrypted data
-				byte[] ba = ms.ToArray();
-				fs.Write(ba, 0, ba.Length);
-			}
-			finally
-			{
-				if (fs != null)
-					fs.Close();
-			}
+                using (FileStream fs = File.OpenWrite(filename))
+                {
+                    // Write the salt to the beginning of the file
+                    fs.Write(salt, 0, salt.Length);
+                    // Write the encrypted data
+                    byte[] ba = ms.ToArray();
+                    fs.Write(ba, 0, ba.Length);
+                }
+            }
+        }
 
-			return;
-		}
-	
-		/// <summary>
-		/// Retrieve the string data from an encrypted file.   Retrieve assumes 
-		/// that Create was used to create the file.
-		/// </summary>
-		/// <param name="filename">File name to retrieve encrypted data from.</param>
-		/// <param name="pswd">Password phrase used when data was encrypted.</param>
-		/// <returns>Unencrypted string contents of file</returns>
-		static public string Retrieve(string filename, string pswd)
-		{
-			FileStream fs = null;
-			byte[] salt = new byte[IV_SIZE];
-			byte[] enc = null;
-			try
-			{
-				fs = File.OpenRead(filename);
-				fs.Read(salt, 0, salt.Length);
-				enc = new byte[fs.Length - salt.Length];
-				fs.Read(enc, 0, enc.Length);
-			}
-			finally
-			{
-				if (fs != null)
-					fs.Close();
-			}
+        /// <summary>
+        /// Retrieve the string data from an encrypted file.   Retrieve assumes 
+        /// that Create was used to create the file.
+        /// </summary>
+        /// <param name="filename">File name to retrieve encrypted data from.</param>
+        /// <param name="pswd">Password phrase used when data was encrypted.</param>
+        /// <returns>Unencrypted string contents of file</returns>
+        static public string Retrieve(string filename, string pswd)
+        {
+            byte[] salt = new byte[IV_SIZE];
+            byte[] enc;
 
-			// create the key from the password phrase
-			PasswordDeriveBytes pdb = new PasswordDeriveBytes(pswd, salt);
-			byte[] key = pdb.GetBytes(KEY_SIZE);
+            using (FileStream fs = File.OpenRead(filename))
+            {
+                fs.Read(salt, 0, salt.Length);
+                enc = new byte[fs.Length - salt.Length];
+                fs.Read(enc, 0, enc.Length);
+            }
 
-			// Create an instance of the RijndaelManaged class
-			RijndaelManaged rm = new RijndaelManaged();
-			CryptoStream cs = null;
-			MemoryStream ms = null;
-			MemoryStream ms2 = null;
-			string outdata=null;
-			try
-			{
-				ms = new MemoryStream(enc);		// memory stream to read encrypted data from
-				ms2 = new MemoryStream();		// decrypted memory stream
-				cs = new CryptoStream(ms, rm.CreateDecryptor(key, salt), CryptoStreamMode.Read);
-				byte[] ta = new byte[256];
-				int count=0;
-				while (true)
-				{
-					count = cs.Read(ta, 0, ta.Length);
-					if (count <= 0)
-						break;
-					ms2.Write(ta, 0, count);
-				}
-				ta = ms2.ToArray();
-				outdata = Encoding.UTF8.GetString(ta);
-			}
-			finally
-			{
-				if (cs != null)
-					cs.Close();
-				if (ms != null)
-					ms.Close();
-				if (ms2 != null)
-					ms2.Close();
-			}
-			
-			return outdata;
-		}
-	}
+            // create the key from the password phrase
+            var pdb = new Rfc2898DeriveBytes(pswd, salt, 10000, HashAlgorithmName.SHA256);
+            byte[] key = pdb.GetBytes(KEY_SIZE);
+
+            // Create an instance of the Aes class
+            using (Aes aes = Aes.Create())
+            using (MemoryStream ms = new MemoryStream(enc))
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(key, salt), CryptoStreamMode.Read))
+            using (MemoryStream ms2 = new MemoryStream())
+            {
+                byte[] ta = new byte[256];
+                int count;
+                while ((count = cs.Read(ta, 0, ta.Length)) > 0)
+                {
+                    ms2.Write(ta, 0, count);
+                }
+                return Encoding.UTF8.GetString(ms2.ToArray());
+            }
+        }
+    }
 }
