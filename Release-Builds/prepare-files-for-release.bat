73ee01f0ac5d3df6f28f01616cb8d45a216cd029
Peter Gill <peter@majorsilence.com>
2025-04-13 00:00:10 -0230

    Cleanup build scripts and documentation
    - Deleted `BundleRdlCmd.sh`
    - Deleted old readme in release build folder
    - Deleted old `prepare-files-for-release.bat` regarding assembly versioning.
    - Deleted `AssemblyInfoUtil.exe`, removing the assembly versioning utility from the project.

--- Release-Builds/prepare-files-for-release.bat
+++
@@ -1,6 +0,0 @@
-
-REM Increment minor number by 1
-REM See http://www.codeproject.com/Articles/31236/How-To-Update-Assembly-Version-Number-Automaticall
-REM See https://github.com/TownSuite/AssemblyInfoUtil
-
-.\tools\AssemblyInfoUtil.exe -inc:2 "%CD%\..\Directory.Build.props"
