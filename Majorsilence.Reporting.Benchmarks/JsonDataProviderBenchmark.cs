35424345aff4c706c1ad5feb97bd4aa51b5168e1
Peter Gill <peter.gill@townsuite.com>
2025-07-26 18:32:44 -0230

    feat: add benchmarking for JSON data provider using BenchmarkDotNet

---
+++ Majorsilence.Reporting.Benchmarks/JsonDataProviderBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System;
+using Majorsilence.Reporting.RdlCreator;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Majorsilence.Reporting.Benchmarks
+{
+    [AsciiDocExporter]
+    [HtmlExporter]
+    [MemoryDiagnoser]
+    [RPlotExporter]
+    [SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+    public class JsonDataProviderBenchmark
+    {
+        string dataProvider = "Json";
+        private string connectionString = "file=NestedJsonData.json";
+        
+        [Benchmark]
+        public async Task NestedJson()
+        {
+            var create = new RdlCreator.Create();
+
+            var fyiReport = await create.GenerateRdl(dataProvider,
+                connectionString,
+                "columns=EmployeeID,LastName,FirstName,ContactInfo_Phone,ContactInfo_Email",
+                pageHeaderText: "DataProviderTest TestMethod1");
+            using var ms = new Majorsilence.Reporting.Rdl.MemoryStreamGen();
+            await fyiReport.RunGetData(null);
+            await fyiReport.RunRender(ms, Majorsilence.Reporting.Rdl.OutputPresentationType.CSV);
+            var text = ms.GetText();
+        }
+    }
+}
---
+++ Majorsilence.Reporting.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Majorsilence.Reporting.Benchmarks;
+
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly, 
+    new DebugBuildConfig()
+        .WithOptions(ConfigOptions.DisableOptimizationsValidator));
+
+Console.WriteLine(summary);
